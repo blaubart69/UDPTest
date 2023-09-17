c0b6fd8a6dab67d5ecfd4c2f090ef03392dbc0e6
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2023-09-17 11:25:10 +0200

    more ways to create a socket

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,29 @@
 use std::error::Error;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, IpAddr};
 use std::sync::Arc;
 use std::{env,io};
 
 use tokio::net::UdpSocket;
 
-async fn receive(addr: Arc<String>) -> Result<(), io::Error> {
+
+//
+// we have to set ReUse BEFORE we do the bind()
+//  otherwise it dosn't work
+//
+async fn receive(addr: SocketAddr) -> Result<(), io::Error> {
 
 	let mut buf : Vec<u8> = vec![0; 1024];
 
-    let s2 = socket2::Socket::new(socket2::Domain::IPV4, socket2::Type::DGRAM, None)?;
-    s2.set_reuse_address(true)?;
-    s2.set_reuse_port(true)?;
-    
-    let address: SocketAddr = addr.parse().unwrap();
-    s2.bind(&address.into())?;
+    let socket = tokio::net::UdpSocket::from_std(
+    {
+        // use socket2 library to set the socketoptions without the use for unsafe
+        let s2 = socket2::Socket::new(socket2::Domain::for_address(addr), socket2::Type::DGRAM, None)?;
+        s2.set_reuse_address(true)?;
+        s2.set_reuse_port(true)?;
+        s2.bind(&addr.into())?;     // bind() have to be AFTER REUSE!!!
+        s2.into()
+    })?;
 
-    //let socket = UdpSocket::bind(&addr.as_str()).await?;
-    let socket = tokio::net::UdpSocket::from_std(s2.into())?;
-    
     println!("bound to: {}", socket.local_addr()?);
 
 	loop {
@@ -31,19 +36,20 @@ async fn receive(addr: Arc<String>) -> Result<(), io::Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let addr  = env::args()
+    let str_ip  = env::args()
         .nth(1)
-        .unwrap_or_else(|| "0.0.0.0:8080".to_string());
+        .unwrap_or_else(|| "0.0.0.0".to_string());
 
-    let arc_addr = Arc::new(addr);
+    let ip = str_ip.parse::<IpAddr>()?;
 
     let mut receive_futues = Vec::new();
     for _ in 0..3  
     {
+        let sock_addr = std::net::SocketAddr::new(ip, 34000);
         println!("spawning recv...");
         receive_futues.push( 
             tokio::spawn(
-                receive(arc_addr.clone() )));
+                receive(sock_addr.clone())));
     }
 
     println!("waiting for receives...");
